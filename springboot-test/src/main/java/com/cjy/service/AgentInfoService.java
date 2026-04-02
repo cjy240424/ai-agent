1a560cf5c485a3341081b5a0692155a305efac14
cjy240424 <167969827+cjy240424@users.noreply.github.com>
2026-04-02 21:25:34 +0800

    Delete springboot-test directory

--- springboot-test/src/main/java/com/cjy/service/AgentInfoService.java
+++
@@ -1,8 +0,0 @@
-package com.cjy.service;
-
-import com.baomidou.mybatisplus.extension.service.IService;
-import com.cjy.pojo.AgentInfo;
-
-public interface AgentInfoService extends IService<AgentInfo> {
-    AgentInfo agentSearchById(Integer id);
-}
